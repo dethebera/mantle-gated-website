212460da82d1860159d4879b26070134d2f7aaf9
Utkarsh <54569941+utkarshxy@users.noreply.github.com>
2023-04-13 13:11:51 +0000

    changed custom config to supported config

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,35 +9,14 @@ import { MantleTestnet } from "@thirdweb-dev/chains";
 //const activeChainId = ChainId.MantleTestnet;
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThirdwebProvider
-      //activeChain={MantleTestnet}
-
-      activeChain={{
-        // === Required information for connecting to the network === \\
-        chainId: 5001, // Chain ID of the network
-        // Array of RPC URLs to use
-        rpc: ["https://rpc.testnet.mantle.xyz"],
-
-        // === Information for adding the network to your wallet (how it will appear for first time users) === \\
-        // Information about the chains native currency (i.e. the currency that is used to pay for gas)
-        nativeCurrency: {
-          decimals: 18,
-          name: "Testnet BitDAO",
-          symbol: "BIT",
-        },
-        shortName: "mantle-testnet", // Display value shown in the wallet UI
-        slug: "Mantle-Testnet", // Display value shown in the wallet UI
-        testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
-        chain: "ETH", // Name of the network
-        name: "Mantle Testnet", // Name of the network
-      }}
-  
-      authConfig={{
-        domain: domainName,
-        authUrl: "/api/auth",
-      }}
-    >
+    return (
+      <ThirdwebProvider
+        activeChain={MantleTestnet}
+        authConfig={{
+          domain: domainName,
+          authUrl: "/api/auth",
+        }}
+      >
       <Head>
         <title>Mantle Ecosystem Grants</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
